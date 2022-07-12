a8434fb27a3e4583daf80c850c95062a80f72864
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-07-12 13:31:21 -0400

    add Heroku

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
-
 const app = express();
+const PORT = process.env.PORT || 3001;
+// parse incoming data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON
+app.use(express.json());
+//look in public folder
+app.use(express.static('public'));
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+require('./routes/apiroutes')(app);
+require('./routes/htmlroutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
